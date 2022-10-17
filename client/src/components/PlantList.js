8a4b3cd1f34cbd9284fcc82a8f9f0c8dda125f80
Terry Pinn <terrypinn27@gmail.com>
2022-10-17 18:18:10 +0200

    Remove disused import statement

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 import * as datefns from 'date-fns'
 
-import Box from '@mui/material/Box';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
