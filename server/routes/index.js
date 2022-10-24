e17eb3fd9da8ec0257014b27a80c80c8035bb814
Terry Pinn <terrypinn27@gmail.com>
2022-10-24 09:43:56 +0200

    Change api base url response

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function (req, res) {
-  res.redirect('/plants');
+  res.json({ message: 'Welcome to grow-log application' });
 });
 
 module.exports = router;
