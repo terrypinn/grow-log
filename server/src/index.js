103a002bab51c289df0eed3346667fa37a3a9556
Terry Pinn <terrypinn27@gmail.com>
2022-11-14 11:56:04 +0200

    Move server code to src folder

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: require('path').resolve(__dirname, '.env') });
+require('dotenv').config({ path: require('path').resolve(__dirname, '..', '.env') });
 const app = require('./server');
 const dbo = require('./dbo');
 
