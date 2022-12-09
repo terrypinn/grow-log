89a417127cf5c4718bde041328fc9c930abc6a1d
Terry Pinn <terrypinn27@gmail.com>
2022-12-09 16:10:50 +0200

    Create and use log form component

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,9 @@ import PlantList from './components/PlantList';
 import PlantAddEdit from './components/PlantAddEdit';
 import LogList from './components/LogList';
 import LogAddEdit from './components/LogAddEdit';
+
 import { ACTIONS as PLANT_FORM_ACTIONS } from './components/PlantForm';
+import { ACTIONS as LOG_FORM_ACTIONS } from './components/LogForm';
 
 function App() {
   return (
@@ -16,8 +18,8 @@ function App() {
           <Route exact path="/plant/add" element={<PlantAddEdit action={PLANT_FORM_ACTIONS.add} />} />
           <Route exact path="/plant/edit" element={<PlantAddEdit action={PLANT_FORM_ACTIONS.edit} />} />
           <Route exact path="/logs" element={<LogList />} />
-          <Route exact path="/log/add" element={<LogAddEdit mode="add" />} />
-          <Route exact path="/log/edit" element={<LogAddEdit mode="edit" />} />
+          <Route exact path="/log/add" element={<LogAddEdit action={LOG_FORM_ACTIONS.add} />} />
+          <Route exact path="/log/edit" element={<LogAddEdit action={LOG_FORM_ACTIONS.edit} />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ client/src/components/LogForm.js
@@ -0,0 +1,220 @@
+import { LOG_TYPE, LOG_TYPE_OPTIONS, PLANT_STAGE } from '../constants';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router';
+import { differenceInDays } from 'date-fns'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Typography from '@mui/material/Typography';
+import CancelIcon from '@mui/icons-material/Cancel';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SaveIcon from '@mui/icons-material/Save';
+
+export const ACTIONS = {
+  add: 'add',
+  edit: 'edit'
+};
+
+const NOTE_OPTIONS_DELIMITER = ' | ';
+
+LogForm.defaultProps = {
+  action: ACTIONS.add,
+  log: null
+};
+
+export default function LogForm(props) {
+  const { action, plant, log } = props;
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    created_on: new Date(),
+    type: '',
+    stage: action === ACTIONS.add ? plant.stage : '',
+    note: '',
+    images: ''
+  });
+
+  useEffect(() => {
+    if (action === ACTIONS.edit) {
+      const form = {
+        ...log,
+        images: log.images.join('\n')
+      };
+      setForm(form);
+    }
+  }, [action]);
+
+  const updateForm = value => setForm(prev => ({ ...prev, ...value }));
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    const body = {
+      ...form,
+      created_on: +form.created_on,
+      day: plant.started_on ? differenceInDays(form.created_on, plant.started_on).toString() : '',
+      images: form.images.split(/\r?\n/).filter(x => x !== ''),
+      plant_id: plant._id
+    };
+    action === ACTIONS.add 
+      ? createLog(body)
+      : updateLog(body);
+  };
+
+  const createLog = (body) =>
+    fetch(`${process.env.REACT_APP_API_URL}/log`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body),
+    }).then(() => navigate(-1));
+
+  const updateLog = (body) =>
+    fetch(`${process.env.REACT_APP_API_URL}/log/${log._id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body),
+    }).then(() => navigate(-1));
+
+  const deleteLog = () => {
+    if (!window.confirm('Are you sure you want to delete this log?')) return;
+    fetch(`${process.env.REACT_APP_API_URL}/log/${log._id}`, { method: 'DELETE' }).then(() => navigate(-1));
+  };
+
+  const onChangeType = (value) => {
+    const options = LOG_TYPE_OPTIONS[value];
+    if (!options) {
+      updateForm({ type: value });
+      return;
+    }
+    const dialog = () => window.confirm('This action will clear your note. Proceed?');
+    if (!form.note.trim() || dialog()) updateForm({ type: value, note: options.join(NOTE_OPTIONS_DELIMITER) });
+  };
+
+  return (
+    <Box
+      component="form"
+      noValidate
+      autoComplete="off"
+      onSubmit={submitForm}
+    >
+      <Grid container alignItems="center">
+        <Grid item xs={action === ACTIONS.add  ? 12 : 10}>
+          <Typography variant="h6">
+            {action === ACTIONS.add  ? 'Add' : 'Edit'} Log ({plant.name})
+          </Typography>
+        </Grid>
+        {action === ACTIONS.edit &&
+          <Grid item xs={2}>
+            <Box display="flex" justifyContent="flex-end">
+              <IconButton aria-label="delete" size="large" onClick={deleteLog}>
+                <DeleteIcon fontSize="inherit" />
+              </IconButton>
+            </Box>
+          </Grid>}
+      </Grid>
+
+      <Grid container spacing={1} mt={1}>
+        <Grid item xs={4}>
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <DateTimePicker
+              label="Created On"
+              value={form.created_on}
+              inputFormat="dd/MM/yyyy HH:mm"
+              onChange={value => updateForm({ created_on: value })}
+              renderInput={params => <TextField {...params} fullWidth />}
+            />
+          </LocalizationProvider>
+        </Grid>
+        <Grid item xs={4}>
+          <FormControl fullWidth>
+            <InputLabel id="log-type-select-label">Type</InputLabel>
+            <Select
+              labelId="log-type-select-label"
+              id="log-type-select"
+              value={form.type}
+              label="Type"
+              onChange={e => onChangeType(e.target.value)}
+            >
+              {Object.keys(LOG_TYPE).map(x => {
+                return <MenuItem key={x} value={x}>{LOG_TYPE[x]}</MenuItem>
+              })}
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={4}>
+          <FormControl fullWidth>
+            <InputLabel id="stage-select-label">Stage</InputLabel>
+            <Select
+              labelId="stage-select-label"
+              id="stage-select"
+              value={form.stage}
+              label="Stage"
+              onChange={e => updateForm({ stage: e.target.value })}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              {Object.keys(PLANT_STAGE).map(x => {
+                return <MenuItem key={x} value={x}>{PLANT_STAGE[x]}</MenuItem>
+              })}
+            </Select>
+          </FormControl>
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={1} mt={1}>
+        <Grid item xs={12}>
+          <TextField
+            id="note"
+            label="Note"
+            fullWidth
+            multiline
+            rows={12}
+            value={form.note}
+            onChange={e => updateForm({ note: e.target.value })}
+          />
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={1} mt={1}>
+        <Grid item xs={12}>
+          <TextField
+            id="images"
+            label="Images"
+            fullWidth
+            multiline
+            rows={4}
+            value={form.images}
+            onChange={e => updateForm({ images: e.target.value })}
+          />
+        </Grid>
+      </Grid>
+
+      <Box mt={1} spacing={1}>
+        <Button
+          type="submit"
+          variant="contained"
+          startIcon={<SaveIcon />}
+          sx={{ mr: 1 }}
+        >
+          Save
+        </Button>
+        <Button
+          variant="outlined"
+          onClick={() => navigate(-1)}
+          startIcon={<CancelIcon />}
+        >
+          Cancel
+        </Button>
+      </Box>
+    </Box>
+  );
+}
