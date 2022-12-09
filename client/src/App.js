10b0dae353cec0f4fbc294c91938e18f7a5a3e81
Terry Pinn <terrypinn27@gmail.com>
2022-12-09 15:47:43 +0200

    Use form component

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import PlantList from './components/PlantList';
 import PlantAddEdit from './components/PlantAddEdit';
 import LogList from './components/LogList';
 import LogAddEdit from './components/LogAddEdit';
+import { ACTIONS as PLANT_FORM_ACTIONS } from './components/PlantForm';
 
 function App() {
   return (
@@ -12,8 +13,8 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Navigate to="/plants" />} />
           <Route exact path="/plants" element={<PlantList />} />
-          <Route exact path="/plant/add" element={<PlantAddEdit mode="add" />} />
-          <Route exact path="/plant/edit" element={<PlantAddEdit mode="edit" />} />
+          <Route exact path="/plant/add" element={<PlantAddEdit action={PLANT_FORM_ACTIONS.add} />} />
+          <Route exact path="/plant/edit" element={<PlantAddEdit action={PLANT_FORM_ACTIONS.edit} />} />
           <Route exact path="/logs" element={<LogList />} />
           <Route exact path="/log/add" element={<LogAddEdit mode="add" />} />
           <Route exact path="/log/edit" element={<LogAddEdit mode="edit" />} />
