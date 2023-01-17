0ad86fc8fc7a52184861b2fcce00a538e55f0777
Terry Pinn <terrypinn27@gmail.com>
2023-01-17 20:03:20 +0200

    Add view to list archived plants only

---
+++ client/src/components/ArchiveList.js
@@ -0,0 +1,17 @@
+import { PLANT_STAGE } from '../constants';
+import { useEffect, useState } from 'react';
+import PlantTable from './PlantTable';
+
+export default function ArchiveList() {
+  const [plants, setPlants] = useState([]);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}/plants`)
+      .then(resppnse => resppnse.json())
+      .then(plants => setPlants(plants.filter(x => x.stage === PLANT_STAGE.Archive)));
+  }, []);
+
+  return (
+    <PlantTable plants={plants} />
+  );
+}
